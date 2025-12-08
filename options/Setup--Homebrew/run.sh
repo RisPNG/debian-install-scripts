e7d53d8980beee9ce3c6328d2bc4ebc16c7d4ec2
Ris <ris@poppybit.com>
2025-12-08 09:49:35 +0800

    safe append

--- options/Setup--Homebrew/run.sh
+++ options/Setup--Homebrew/run.sh
@@ -1,9 +1,9 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-SHELL_RC="${HOME}/.bashrc"
-if ! grep -q 'brew shellenv' "$SHELL_RC"; then
-    echo >> "$SHELL_RC"
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$SHELL_RC"
+LINE='eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+
+if ! grep -Fxq "$LINE" ~/.bashrc; then
+  echo "$LINE" >> ~/.bashrc
 fi
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install gcc
