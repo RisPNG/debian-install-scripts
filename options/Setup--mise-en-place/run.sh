e7007547d19d2d34f4b68e688eb88df5c9b7c603
RisPNG <nikfarisaiman109@hotmail.com>
2026-01-23 10:35:57 +0800

    remove wkhtmltopdf

--- options/Setup--mise-en-place/run.sh
+++ options/Setup--mise-en-place/run.sh
@@ -1,7 +1,4 @@
 sudo apt install autoconf build-essential curl flex fop gcc git icu-devtools inotify-tools libcurl4-openssl-dev libedit-dev libgl1-mesa-dev libglu1-mesa-dev libicu-dev libncurses-dev libpam0g-dev libpng-dev libreadline-dev libssh-dev libssl-dev libwxgtk-webview3.2-dev libwxgtk3.2-dev libxml2-dev libxml2-utils libxslt1-dev m4 make unixodbc-dev unzip uuid-dev xsltproc zlib1g-dev bison -y
-download_url=$(curl -fsSL https://api.github.com/repos/newinnovations/wkhtml-packaging/releases/latest | jq -r '.assets[] | select(.name | endswith("trixie_amd64.deb")) | .browser_download_url')
-wget "$download_url" -O ~/Downloads/wkhtmltopdf.deb
-sudo apt install ~/Downloads/wkhtmltopdf.deb -y
 
 curl https://mise.run | sh
 if ! grep -q 'mise activate' ~/.bashrc; then
